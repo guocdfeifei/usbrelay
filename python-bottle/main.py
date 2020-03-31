571f1cd125f2438be2558933247a71d8e6d42bd3
guoff <guocdfeifei@163.com>
2020-03-31 17:47:29 +0800

    优化

--- python-bottle/main.py
+++ python-bottle/main.py
@@ -43,26 +43,22 @@ def index():
 def server_Static(filename):
     return static_file(filename, root='./')
 
+def updateRelay(relay,state):
+    before = GPIO.input(relay)
+    flag=True
+    while flag and before!=state:
+        GPIO.output(relay, int(state))
+        after = GPIO.input(relay)
+        if before!=after:
+            flag=False
+
 @route('/Relay', method="POST")
 def Relay_Control():
   global Relay1,Relay2,Relay3,Relay4,Relay5,Relay6,Relay7,Relay8
   
-  Relay1 = request.POST.get('Relay1')
-  Relay2 = request.POST.get('Relay2')
-  Relay3 = request.POST.get('Relay3')
-  Relay4 = request.POST.get('Relay4')
-  Relay5 = request.POST.get('Relay5')
-  Relay6 = request.POST.get('Relay6')
-  Relay7 = request.POST.get('Relay7')
-  Relay8 = request.POST.get('Relay8')
-  GPIO.output(Relay[0], int(Relay1))
-  GPIO.output(Relay[1], int(Relay2))
-  GPIO.output(Relay[2], int(Relay3))
-  GPIO.output(Relay[3], int(Relay4))
-  GPIO.output(Relay[4], int(Relay5))
-  GPIO.output(Relay[5], int(Relay6))
-  GPIO.output(Relay[6], int(Relay7))
-  GPIO.output(Relay[7], int(Relay8))
+  Relay1 = request.POST.get('Relay')
+  RelayState = request.POST.get('RelayState')
+  updateRelay(Relay[int(Relay1)-1], int(RelayState))
 
   
 run(host="0.0.0.0", port=8080)
